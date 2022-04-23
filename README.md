e7c02848701e843f3f5e75da9ce0f07540286d38
Saber <saberkhakbiz73@gmail.com>
2022-04-23 20:50:23 +0430

    update app.sh file

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # Bash Script
 ### A Script can handle file in linux directories :)
 
+https://img.shields.io/github/license/saber-khakbiz/Bash-MK-Project?style=for-the-badge
 
-![GitHub](https://github.com/saber-khakbiz/Bash-MK-Project/blob/master/LICENSE?style=flat-square)
 
 ![img](https://github.com/saber-khakbiz/Bash-MK-Project/blob/master/img_src/bash_logo.png)
 
--- app.sh
+++ app.sh
@@ -1,1 +1,27 @@
 #!/bin/usr/env bash
+
+files=$(eval ls -AF) 
+
+for file in $files;do
+
+	if [ "${file}" == "${0##*/}" ];then
+		continue
+	fi
+	
+	if [[  "$file" =~ .*[\.].* ]];then	
+		suffix="$(echo ${file} |grep -Pow '[\w\.]*[\.]{1}\K[\w]*' \
+			|tr [:lower:] [:upper:])""_FILE"
+	fi
+	
+	if [[ "$file" =~ .*[\.].* ]];then
+		if [ -d "$suffix" ];then
+			mv $file "$suffix"
+		else
+	
+			mkdir "$suffix"
+			mv $file "$suffix"
+		
+		fi
+	fi
+	
+done
