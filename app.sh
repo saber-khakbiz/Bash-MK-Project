4a9b78eaecf81b3333c827ff6faa58bb28b4c9f4
Saber <saberkhakbiz73@gmail.com>
2022-04-24 12:03:01 +0430

    add comments to script

--- app.sh
+++ app.sh
@@ -1,26 +1,34 @@
-#!/bin/usr/env bash
+#!/usr/bin/env bash
 
+
+#Read All of files in current Directory
 files=$(eval ls -AF) 
 
+
 for file in $files;do
+
+#	ignore this file {app.sh}
 
 	if [ "${file}" == "${0##*/}" ];then
 		continue
 	fi
 	
+#	check if that is a file {not a directory or file with no Extension}
+#	then grep suffix and upper and add {_FILES} to it!
+
 	if [[  "$file" =~ .*[\.].* ]];then	
 		suffix="$(echo ${file} |grep -Pow '[\w\.]*[\.]{1}\K[\w]*' \
-			|tr [:lower:] [:upper:])""_FILE"
+			|tr [:lower:] [:upper:])""_FILES"
 	fi
 	
+
 	if [[ "$file" =~ .*[\.].* ]];then
+
 		if [ -d "$suffix" ];then
 			mv $file "$suffix"
 		else
-	
 			mkdir "$suffix"
 			mv $file "$suffix"
-		
 		fi
 	fi
 	
